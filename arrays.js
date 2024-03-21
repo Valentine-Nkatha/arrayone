9e2459dd31a682127504659b491fdb9d9c0b0556
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-03-21 13:14:26 +0300

    added files

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let arr1=[3,7,34,90,12];
+console.log("last element",arr1.slice(-1));
+let arr2=[true,"green","where",12,56];
+console.log("last element",arr2.slice(-1));
+
+//QUESTION2
+let myPets=["Cow","Bird","Snake","Dog"];
+let myPets2=myPets.join();
+console.log({myPets2});
+//QUESTION3
+var arr3=[-5,9,5,3,2,-3,6,8,4,1];
+var arr4=arr3.sort();
+console.log({arr4});
+//QUESTION 4
+
